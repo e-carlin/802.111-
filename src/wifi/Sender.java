44cc84b38e7abdcb7b15dee05ed6418b48070ffc
Evan Carlin <evan@carlin.com>
2016-04-04 14:12:12 -0700

    added waiting for idle channel

--- src/wifi/Sender.java
+++ src/wifi/Sender.java
@@ -22,19 +22,28 @@ public class Sender implements Runnable {
 	@Override
 	public void run() {
 		while(true){
-			
+
 			//****Neeed to wait to make sure channel is free
 			while(!dataToTrans.isEmpty()){ //while there is data to transmit
-				System.out.println("Sending "+ Arrays.toString(dataToTrans.get(0))); //***TESTING
-				this.theRF.transmit(dataToTrans.get(0)); //transmit the data
-				dataToTrans.removeElementAt(0); //Delete the packet because it has been sent
+				if(!theRF.inUse()){
+					System.out.println("Sending "+ Arrays.toString(dataToTrans.get(0))); //***TESTING
+					this.theRF.transmit(dataToTrans.get(0)); //transmit the data
+					dataToTrans.removeElementAt(0); //Delete the packet because it has been sent
+				}
+				try{ //Sleep the thread a bit before checking for idle channel
+					Thread.sleep(200); //Wait .5 second
+				}
+				catch(InterruptedException e){ //If interrupted during sleep
+					System.out.println("Interrupted while waiting for idle channel "+e);
+
+				}
 			}
 
 			try{ //Sleep the thread a bit before checking again for new data
-				Thread.sleep(500); //Wait .5 second
+				Thread.sleep(200); //Wait .5 second
 			}
 			catch(InterruptedException e){ //If interrupted during sleep
-				System.out.println("Broadcast Interrupted "+e);
+				System.out.println("Interrupted while waiting for data to brodcast "+e);
 
 			}
 		}
