ef554f42af10d864ff85bdc052f954da3fc8236e
Evan Carlin <evan@carlin.com>
2016-04-13 20:29:41 -0700

    big commit

--- src/wifi/Sender.java
+++ src/wifi/Sender.java
@@ -1,6 +1,7 @@
 package wifi;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import rf.RF;
@@ -16,6 +17,7 @@ public class Sender implements Runnable {
 	private ConcurrentLinkedQueue<byte[]> dataToTrans;
 	private ConcurrentLinkedQueue<byte[]> rcvdACK;
 
+	private int collisions = this.theRF.aCWmin; //The number of collisions that have occurred
 	private final int TIMEOUT = 1000; //milliseconds - This is completely made up need better number
 	private final int DIFS = 50/1000; //Also made up
 
@@ -25,23 +27,7 @@ public class Sender implements Runnable {
 		this.rcvdACK = rcvdACK;
 	}
 
-	/**
-	 * This function waits a certain interval for an ACK 
-	 * @return Whether or not the ACK was received before the timeout
-	 */
-	private boolean waitForACK(){
 
-		System.out.println("Waiting for ACK");
-		long startTime = LinkLayer.clock();
-		while(LinkLayer.clock() > startTime + TIMEOUT){ //We have not yet reached the timeout
-			while(!rcvdACK.isEmpty()){ //An ACK was rcvd
-				rcvdACK.poll(); //We no longer need this ACK
-				//****Probably should check sequence numbers here
-				return true;
-			}
-		}
-		return false;
-	}	
 
 	/**
 	 * A method that waits DIFS and attempts to transmit a packet
@@ -49,18 +35,41 @@ public class Sender implements Runnable {
 	 */
 	private boolean waitAndSendData(){
 
-		try{ //Sleep the thread for DIFS
-			Thread.sleep(0,50000); //Wait 50 microseconds ******Is that the correct DIFS wait ???*****
-		}
-		catch(InterruptedException e){ //If interrupted during sleep
-			System.out.println("Interrupted while waiting DIFS "+e);
+		boolean keepTrying = true;
+		while(keepTrying){
+			try{ //Sleep the thread for DIFS
+				Thread.sleep(0,50000); //Wait 50 microseconds ******Is that the correct DIFS wait ???*****
+			}
+			catch(InterruptedException e){ //If interrupted during sleep
+				System.out.println("Interrupted while waiting DIFS "+e);
 
-		}
+			}
 
-		if(!theRF.inUse()){ //The channel is still idle
-			this.theRF.transmit(dataToTrans.peek()); //transmit the frame
-			System.out.println("Transmitting data!");
-			return true; //We transmitted the frame so we are done
+			if(!theRF.inUse()){ //The channel is still idle
+				
+				//wait our necessary exponential backoff time pausing whenever channel is in use
+				
+				//Calculating the necessary backoff
+				Random rn = new Random();
+				if(collisions > theRF.aCWmax) //We have maxed out the cw
+					collisions = theRF.aCWmax;
+				int cw = rn.nextInt(collisions - theRF.aCWmin + 1) + theRF.aCWmin;
+				double interval = Math.random()*cw;
+				int wait = (int) (interval * theRF.aSlotTime);
+				
+				try{ //Wait our random time
+					Thread.sleep(wait);
+				}
+				catch(InterruptedException e){ //If interrupted during sleep
+					System.out.println("Interrupted while waiting exponential backoff "+e);
+
+				}
+				
+				//Transmit after waiting
+				this.theRF.transmit(dataToTrans.peek()); //transmit the frame
+				System.out.println("Transmitting data!");
+				return true; //We transmitted the frame so we are done
+			}
 		}
 
 		//The channel was no longer idle so we were unable to send the frame
@@ -68,31 +77,37 @@ public class Sender implements Runnable {
 		return false;
 	}
 
+	private boolean waitForACK(){
+		long startTime = LinkLayer.clock();
+		while(LinkLayer.clock() < (startTime + TIMEOUT)){ //we haven't timed out
+			if(!rcvdACK.isEmpty()){ //We've rcvd an ack
+				//Check to make sure it is the right seq #
+				rcvdACK.poll(); //remove the ACK
+				this.collisions = 0; //Reset the number of collisions because we succesfully transmitted
+				return true; //Our packet has been ACK'ed
+			}
 
+		}
+		return false; //we timed out
+	}
 
 	@Override
 	public void run() {
 		while(true){
 
 			while(!dataToTrans.isEmpty()){ //while there is data to transmit
-				if(!theRF.inUse()){ //If the channel is idle then begin otherwise keep waiting until it is idle
-					if(waitAndSendData()){ //try to send the packet
-						System.out.println("Sent data");
-						if(waitForACK()){ //We rcvd an ACK for this packet
-							dataToTrans.poll(); //This packet has been ACK'ed so we can get rid of it
-							System.out.println("Removed old packet");
-							break; //All done with current packet
-						}
-						//if no ACK after timeout there was a collision so call another routine
+				if(waitAndSendData()){ //try to send the packet
+					if(waitForACK()){ //After sending the packet wait for an ACK
+						dataToTrans.poll(); //Remove this packet
+						break; //We've rcvd the ACK so were all done with this packet!
 					}
-					//waited DIFS and then the channel was no longer idle.
-					else 
-						break; //start waiting for channel to be idle again
+					else{ //We didn't rcv an ACK so there must have been a collision
+						//Exponential backoff and retransmit
+						collisions ++; //Increment the collision counter
+						waitAndSendData();
+					}
 				}
-
-
 			}
-
 			try{ //Sleep the thread a bit before checking again for new data
 				Thread.sleep(100); //Wait .1 second
 			}
@@ -104,4 +119,9 @@ public class Sender implements Runnable {
 
 	}
 
+
+
 }
+
+
+
